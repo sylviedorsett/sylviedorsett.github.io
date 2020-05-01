d73d3fb59fe3265214cb807d8a5ba888680d9b4b
sylviedorsett <sylviejovet@hotmail.com>
2020-04-30 18:38:43 -0700

    folders

---
+++ lesson-01/js/index.js
@@ -0,0 +1,6 @@
+var mod = document.lastModified;
+document.getElementById("last").innerHTML = mod;
+
+var date = new Date();
+var currentYear = date.getFullYear();
+document.getElementById("year").innerHTML = currentYear;
