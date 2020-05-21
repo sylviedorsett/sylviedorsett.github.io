5fa4370af98bed7faa0f3beedc074d93901dccb5
sylviedorsett <sylviejovet@hotmail.com>
2020-05-21 15:14:16 -0700

    week 5

--- lesson-05/js/banner.js
+++ lesson-05/js/banner.js
@@ -1,13 +1,31 @@
 var d = new Date ();
 var date = d.getDate();
 var day = d.getDay();
+var month = d.getMonth();
+var currentYear = d.getFullYear();
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-const banner = document.getElementById("banner")
 
-if (day == 4) {
+/* This is the js for the year copywrite.*/
+document.getElementById("year").innerHTML = currentYear;
+
+
+/* This is the js for the banner.*/
+const banner = document.getElementById("banner")
+if (day == 5) {
      banner.style.display = "block";
 }
 else {
     banner.style.display = "none";
 }
+
+/* This is the js for the hamburger menu.*/
+function toggleMenu() {
+    document.getElementById("navigation").classList.toggle("response");
+}
+
+
+/* This is the js for the current date in the footer*/
+var dateString = dayNames[day] + ", " + monthNames[month] + " " + date + ", " + currentYear; 
+document.getElementById(".date").innerHTML = dateString;
