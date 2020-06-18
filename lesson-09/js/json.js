5aaa70047a8c7de45d2c7147744feec23f5fd6f5
sylviedorsett <sylviejovet@hotmail.com>
2020-06-18 14:23:10 -0700

    changes

--- lesson-09/js/json.js
+++ lesson-09/js/json.js
@@ -6,106 +6,41 @@ fetch(requestURL)
     })
     .then(function (jsonObject) {
         const towns = jsonObject['towns']; //stores the results of converted response into an array
-        const preston = towns.filter(town => (town.name == 'Preston'));
-        const sodaSprings = towns.filter(town => (town.name == 'Soda Springs'));
-        const fishHaven = towns.filter(town => (town.name == 'Fish Haven'));
 
-        preston.forEach(town => {
-            let div1 = document.createElement('div');
-            let div2 = document.createElement('div');
-            let image = document.createElement('img'); 
-            let sect = document.createElement('section');
-            let h2 = document.createElement('h2');
-            let p1 = document.createElement('p');
-            let p2 = document.createElement('p');
-            let p3 = document.createElement('p'); 
-            let p4 = document.createElement('p');
+        towns.forEach(town => {
+            if (town.name == 'Preston' || town.name == 'Soda Springs' || town.name == 'Fish Haven') {
 
-            h2.innerHTML = `${town.name}`;
-            p1.innerHTML = `"<em>${town.motto}</em>"`;
-            p2.innerHTML = `Year Founded: ${town.yearFounded}`;
-            p3.innerHTML = `Current Population: ${town.currentPopulation}`;
-            p4.innerHTML = `Average Rainfall in Inches: ${town.averageRainfall}`;
-            image.innerHTML = town.photo;
-            image.setAttribute('src', `images/${town.photo}`);
-            image.setAttribute('alt', `images/${town.photo}`);
+                let div1 = document.createElement('div');
+                let div2 = document.createElement('div');
+                let image = document.createElement('img'); 
+                let sect = document.createElement('section');
+                let h2 = document.createElement('h2');
+                let p1 = document.createElement('p');
+                let p2 = document.createElement('p');
+                let p3 = document.createElement('p'); 
+                let p4 = document.createElement('p');
 
-            div1.appendChild(div2);
-            div2.appendChild(image);
-            div1.appendChild(sect);
-            sect.appendChild(h2);
-            sect.appendChild(p1);
-            sect.appendChild(p2);
-            sect.appendChild(p3);
-            sect.appendChild(p4);
-            document.querySelector('div.jsonDivs').appendChild(div1);
-        });
+                h2.innerHTML = town.name;
+                p1.innerHTML = `"<em>${town.motto}</em>"`;
+                p2.innerHTML = `Year Founded: ${town.yearFounded}`;
+                p3.innerHTML = `Current Population: ${town.currentPopulation}`;
+                p4.innerHTML = `Average Annual Rainfall: ${town.averageRainfall}`;
+                image.innerHTML = town.photo;
+                image.setAttribute('src', `images/${town.photo}`);
+                image.setAttribute('alt', `images/${town.photo}`);
 
-        sodaSprings.forEach(town => {
-            let div1 = document.createElement('div');
-            let div2 = document.createElement('div');;
-            let image = document.createElement('img'); 
-            let sect = document.createElement('section');
-            let h2 = document.createElement('h2');
-            let p1 = document.createElement('p');
-            let p2 = document.createElement('p');
-            let p3 = document.createElement('p'); 
-            let p4 = document.createElement('p');
+                div1.appendChild(div2);
+                div2.appendChild(image);
+                div1.appendChild(sect);
+                sect.appendChild(h2);
+                sect.appendChild(p1);
+                sect.appendChild(p2);
+                sect.appendChild(p3);
+                sect.appendChild(p4);
 
-            h2.innerHTML = `${town.name}`;
-            p1.innerHTML = `"<em>${town.motto}</em>"`;
-            p2.innerHTML = `Year Founded: ${town.yearFounded}`;
-            p3.innerHTML = `Current Population: ${town.currentPopulation}`;
-            p4.innerHTML = `Average Rainfall in Inches: ${town.averageRainfall}`;
-            image.innerHTML = town.photo;
-            image.setAttribute('src', `images/${town.photo}`);
-            image.setAttribute('alt', `images/${town.photo}`);
-
-            div1.appendChild(div2);
-            div2.appendChild(image);
-            div1.appendChild(sect);
-            sect.appendChild(h2);
-            sect.appendChild(p1);
-            sect.appendChild(p2);
-            sect.appendChild(p3);
-            sect.appendChild(p4);
-            document.querySelector('div.jsonDivs').appendChild(div1);
-        });
-
-        fishHaven.forEach(town => {
-            let div1 = document.createElement('div');
-            let div2 = document.createElement('div');
-            let image = document.createElement('img'); 
-            let sect = document.createElement('section');
-            let h2 = document.createElement('h2');
-            let p1 = document.createElement('p');
-            let p2 = document.createElement('p');
-            let p3 = document.createElement('p'); 
-            let p4 = document.createElement('p');
-
-            h2.innerHTML = `${town.name}`;
-            p1.innerHTML = `"<em>${town.motto}</em>"`;
-            p2.innerHTML = `Year Founded: ${town.yearFounded}`;
-            p3.innerHTML = `Current Population: ${town.currentPopulation}`;
-            p4.innerHTML = `Average Rainfall in Inches: ${town.averageRainfall}`;
-            image.innerHTML = town.photo;
-            image.setAttribute('src', `images/${town.photo}`);
-            image.setAttribute('alt', `images/${town.photo}`);
-
-            div1.appendChild(div2);
-            div2.appendChild(image);
-            div1.appendChild(sect);
-            sect.appendChild(h2);
-            sect.appendChild(p1);
-            sect.appendChild(p2);
-            sect.appendChild(p3);
-            sect.appendChild(p4);
-            document.querySelector('div.jsonDivs').appendChild(div1);
+                document.querySelector('div.jsonDivs').appendChild(div1);
+            }
         });
     });
 
-
-/*
-    using backticks newer and easier/cleaner way for concatenation:
-    - `${prophet.name} ${prophet.lastname}`;
-*/
+/*   using backticks newer and easier/cleaner way for concatenation - `${prophet.name} ${prophet.lastname}`; */
