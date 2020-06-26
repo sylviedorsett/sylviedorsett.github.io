b06cc9f002e2245e5e15215ab68314f27a9d2cd9
sylviedorsett <59636797+sylviedorsett@users.noreply.github.com>
2020-06-25 21:08:05 -0700

    Update weatherapi.js

--- lesson-10/js/weatherapi.js
+++ lesson-10/js/weatherapi.js
@@ -6,6 +6,16 @@ fetch(apiURL)
         document.getElementById('temp').textContent = Math.floor(jsObject.main.temp_max);
         document.getElementById('humidity').textContent = jsObject.main.humidity;
         document.getElementById('windSpeed').textContent = Math.ceil(jsObject.wind.speed);
+    
+        var t = parseFloat(document.getElementById("temp").innerHTML);
+        var s = parseFloat(document.getElementById("windSpeed").innerHTML);
+        var chill = 35.74 + .6215 * t - 35.75 * Math.pow(s, .16) +  .4275 * t * Math.pow(s, .16);
+        if (t < 51 && s > 3) {
+            document.getElementById("windChill").innerHTML = Math.round(chill);
+        }
+        else {
+            document.getElementById("windChill").innerHTML = "N/A";
+        }
     });
 
 const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=4491eb92629e7b5e0ac20b732e39129e";
