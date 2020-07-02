0a9c1150aad616b18991a84b5a6d5b11fd632400
sylviedorsett <sylviejovet@hotmail.com>
2020-07-02 14:54:12 -0700

    js files condensed

--- lesson-11/js/prestonJSON.js
+++ lesson-11/js/prestonJSON.js
@@ -1,3 +1,52 @@
+//Weather API
+const prestonapiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=4491eb92629e7b5e0ac20b732e39129e";
+fetch(prestonapiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        document.getElementById('currentTemp').textContent = Math.floor(jsObject.main.temp);
+        console.log(jsObject.main.temp);
+        document.getElementById('temp').textContent = Math.floor(jsObject.main.temp_max);
+        document.getElementById('humidity').textContent = jsObject.main.humidity;
+        document.getElementById('windSpeed').textContent = Math.ceil(jsObject.wind.speed);
+
+        var t = parseFloat(document.getElementById("temp").innerHTML);
+        var s = parseFloat(document.getElementById("windSpeed").innerHTML);
+        var chill = 35.74 + .6215 * t - 35.75 * Math.pow(s, .16) +  .4275 * t * Math.pow(s, .16);
+        if (t < 51 && s > 3) {
+            document.getElementById("windChill").innerHTML = Math.round(chill);
+        }
+        else {
+            document.getElementById("windChill").innerHTML = "N/A";
+        }
+    });
+
+const prestonforecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=4491eb92629e7b5e0ac20b732e39129e";
+fetch(prestonforecastURL)
+    .then((response) => response.json())
+    .then((jsonObject) => {
+
+        const fiveDay = jsonObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+
+        const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        let day = 1;
+
+        fiveDay.forEach(forecast => {
+            let date = forecast.dt_txt;
+            let d = new Date(date).getDay();
+            let f = forecast.main.temp;
+            const icon = `http://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+
+            document.getElementById(`day${day}`).textContent = weekdays[d];
+            document.getElementById(`forecast${day}`).textContent = Math.floor(f);
+            document.getElementById(`imagesrc${day}`).setAttribute('alt', forecast.weather[0].description);
+            document.getElementById(`imagesrc${day}`).setAttribute('src', icon);
+            day++;
+            d++;
+        })
+    });
+    
+//Preston Events:
+
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
 fetch(requestURL)
--- lesson-11/js/sodaspringsJSON.js
+++ lesson-11/js/sodaspringsJSON.js
@@ -1,3 +1,50 @@
+// for Soda Springs
+const ssapiURL = "https://api.openweathermap.org/data/2.5/weather?id=5607916&units=imperial&APPID=4491eb92629e7b5e0ac20b732e39129e";
+fetch(ssapiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        document.getElementById('currentTemp').textContent = Math.floor(jsObject.main.temp);
+        document.getElementById('temp').textContent = Math.floor(jsObject.main.temp_max);
+        document.getElementById('humidity').textContent = jsObject.main.humidity;
+        document.getElementById('windSpeed').textContent = Math.ceil(jsObject.wind.speed);
+
+        var t = parseFloat(document.getElementById("temp").innerHTML);
+        var s = parseFloat(document.getElementById("windSpeed").innerHTML);
+        var chill = 35.74 + .6215 * t - 35.75 * Math.pow(s, .16) +  .4275 * t * Math.pow(s, .16);
+        if (t < 51 && s > 3) {
+            document.getElementById("windChill").innerHTML = Math.round(chill);
+        }
+        else {
+            document.getElementById("windChill").innerHTML = "N/A";
+        }
+    });
+
+const ssforecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5607916&units=imperial&APPID=4491eb92629e7b5e0ac20b732e39129e";
+fetch(ssforecastURL)
+    .then((response) => response.json())
+    .then((jsonObject) => {
+        const fiveDay = jsonObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+        const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        let day = 1;
+
+        fiveDay.forEach(forecast => {
+            let date = forecast.dt_txt;
+            let d = new Date(date).getDay();
+            let f = forecast.main.temp;
+            const icon = `http://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+
+            document.getElementById(`day${day}`).textContent = weekdays[d];
+            document.getElementById(`forecast${day}`).textContent = Math.floor(f);
+            document.getElementById(`imagesrc${day}`).setAttribute('alt', forecast.weather[0].description);
+            document.getElementById(`imagesrc${day}`).setAttribute('src', icon);
+            day++;
+            d++;
+            
+        })
+    });
+
+// Soda Springs Events
+
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
 fetch(requestURL)
